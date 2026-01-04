a525dc609a5b21bfd9c27bccab77c545ddd9970b
Priti Poddar <pritipoddar461@gmail.com>
2026-01-04 22:25:54 +0530

    Initial commit: backend, frontend, dashboard setup

---
+++ backend/middleware.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+// middleware.js
+
+// middleware/auth.js
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next) {
+  const token = req.cookies.token; // Get token from cookies
+  console.log(token);
+
+  if (!token) return res.status(401).json({ error: "Access denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+}
+
+module.exports = verifyToken;
